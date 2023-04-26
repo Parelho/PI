52eab89de4c2eaea220c6d1ffd6ce90b7af38651
Parelho <vinicius.parelho@outlook.com>
2023-04-26 07:46:50 -0300

    Criar

---
+++ Game.py
@@ -0,0 +1,32 @@
+import pygame
+
+pygame.init()
+
+win = pygame.display.set_mode((900,600))
+clock = pygame.time.Clock()
+
+FONT = pygame.font.SysFont("timesnewroman", 50)
+
+class Login:
+    def tela_inicio():
+        bem_vindo = FONT.render("Bem-vindo ao CodeQuiz!", True, "white")
+        win.blit(bem_vindo, (200, 100))
+        cadastrar = FONT.render("Cadastrar", True, "white")
+        win.blit(cadastrar, (500, 300))
+        login = FONT.render("Login", True, "white")
+        win.blit(login, (200, 300))
+        
+
+running = True
+while running == True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    win.fill("lime")
+    clock.tick(60)
+
+    Login.tela_inicio()
+    pygame.display.update()
+
+pygame.quit()
